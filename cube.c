861e51664c0a59eb7880b49242b06b7946d18876
shamykyzer <shamyyk@gmail.com>
2024-07-31 13:24:17 +0300

    added Caclulate X for rotation matrix

--- cube.c
+++ cube.c
@@ -1,1 +1,10 @@
+#include <math.h>
+
+float A, B, C;
+
+float calculateX(int i, int j, int k) {
+return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+j* cos(A) * sin(C) + k* sin(A) * sin(C) +i * cos(B) * cos(C);
+}
+
 int main(){return 0;}
