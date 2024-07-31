f6035db5ccf70c4f615bb2134d36af7aaa621d12
shamykyzer <shamyyk@gmail.com>
2024-07-31 14:14:06 +0300

    figuring out calculatFor Surface and getting side tracked

--- cube.c
+++ cube.c
@@ -7,11 +7,18 @@ float A, B, C;
 
 float cubeWidth = 20;
 int width = 320, height = 88;
-float zBuffer[320][88];
-char buffer[320][88];
+float zBuffer[320*88];
+char *buffer[320*88];
 int backgroundASCIICode = ' ';
 int distanceFromCam = 90;
+float K1 = 40;
+
 float incrementSpeed = 0.8;
+
+float x, y, z;
+float ooz;
+int xp, yp;
+int idx;
 
 float calculateX(int i, int j, int k) {
 return j *sin(A) * sin(B) * cos(C) - k* cos(A) * sin(B) * cos(C) +
@@ -28,6 +35,22 @@ float calcuateZ(int i, int j, int k) {
 return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch){
+    x = calculate(cubeX, cubeY, cubeZ);
+    y = calculate(cubeX, cubeY, cubeZ);
+    z = calculate(cubeX, cubeY, cubeZ);
+
+    float ooz = 1 / z;
+    xp = (int)(width / 2 + K1 * ooz * x * 2);
+    yp = (int)(height / 2 - K1 * ooz * y);
+
+    idx = xp + yp * width;
+    if (idx>=0 && idx<width*height && zBuffer[idx] < ooz) {
+        zBuffer[idx] = ooz;
+        *buffer[idx] = ch;
+    }
+
+}
 
 int main() {
     printf("\x1b[2]");
